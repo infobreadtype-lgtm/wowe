51fac7c7e9ca44eccea9e86ba8e818cbc190c8c6
infobreadtype-lgtm <info.breadtype@gmail.com>
2026-06-18 17:38:00 +0200

    Update wowe.js

--- wowe.js
+++ wowe.js
@@ -1,3 +1,15 @@
-function signUpButtonFunctionJS() {
+let logInDocs = document.getElementById("loginButtonInPage")
+
+let userIsLoggedIn = false
+
+
+
+
+function loginRedirect() {
     window.location.href = 'https://wowe.vercel.app/login.html'
 }
+
+function signUpRedirect() {
+    window.location.href = 'https://wowe.vercel.app/signup.html'
+}
+
