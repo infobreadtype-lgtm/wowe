223ec261b7a103d27510f56b848481d4405a07ad
infobreadtype-lgtm <info.breadtype@gmail.com>
2026-08-08 02:39:43 +0200

    Create redirectToPartners.js

---
+++ redirectToPartners.js
@@ -0,0 +1,7 @@
+const ourPartnersPageFromHome = document.getElementById("ourPartnersMainPage")
+
+ourPartnersPageFromHome.addEventListener("click", ourPartnersRedirect)
+
+function ourPartnersRedirect() {
+    window.location.href = 'https://wowe.vercel.app/partners.html'
+}
