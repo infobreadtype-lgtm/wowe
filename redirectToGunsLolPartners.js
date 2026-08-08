562a130653b6ef5a8e3173b419bfb0ad78c5e7be
infobreadtype-lgtm <info.breadtype@gmail.com>
2026-08-08 02:53:54 +0200

    Create redirectToGunsLolPartners.js

---
+++ redirectToGunsLolPartners.js
@@ -0,0 +1,7 @@
+const gunsLolImagePartnersPage = document.getElementById("gunsDotLolImgPartners")
+
+gunsDotLolImgPartners.addEventListener("click", redirectToGunsLolPartnersPage)
+
+function redirectToGunsLolPartnersPage() {
+    window.location.href = 'https://guns.lol'
+}
