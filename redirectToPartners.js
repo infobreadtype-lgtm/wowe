98e2e1fcbfc98e17d8bd76e9ff66090c477dbdc5
infobreadtype-lgtm <info.breadtype@gmail.com>
2026-08-08 02:53:28 +0200

    Update redirectToPartners.js

--- redirectToPartners.js
+++ redirectToPartners.js
@@ -7,11 +7,3 @@ ourPartnersPageFromHome.addEventListener("click", ourPartnersRedirect)
 function ourPartnersRedirect() {
     window.location.href = 'https://wowe.vercel.app/partners.html'
 }
-
-const gunsLolImagePartnersPage = document.getElementById("gunsDotLolImgPartners")
-
-gunsDotLolImgPartners.addEventListener("click", redirectToGunsLolPartnersPage)
-
-function redirectToGunsLolPartnersPage() {
-    window.location.href = 'https://guns.lol'
-}
